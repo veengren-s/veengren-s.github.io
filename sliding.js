6df504ca6872d45282758cbe6b3f9beaaf3cdfff
veengren-s <71776777+veengren-s@users.noreply.github.com>
2023-06-30 11:56:25 -0600

    sig improvments

--- sliding.js
+++ sliding.js
@@ -1,87 +1,62 @@
 var photo_paths = [
-    "https://i.postimg.cc/dtNnHjnF/DSC00178-Enhanced-NR.jpg",
-    "https://i.postimg.cc/BvnD1Xdv/DSC00200-HDR-Edit.jpg",
-    "https://i.postimg.cc/5NT1cGmN/DSC00506-Enhanced-NR.jpg",
-    "https://i.postimg.cc/JhcWkhCG/DSC00522-Enhanced-NR.jpg",
-    "https://i.postimg.cc/qMz050y5/DSC00538-Enhanced-NR.jpg",
-    "https://i.postimg.cc/jq8tcCwf/DSC00551-Enhanced-NR.jpg",
-    "https://i.postimg.cc/ZnDm5DtH/DSC00553-Enhanced-NR.jpg",
-    "https://i.postimg.cc/zBzYS1qp/DSC00581-Pano-topaz-denoise-sharpen.jpg",
-    "https://i.postimg.cc/kGZrm6Kh/DSC00666.jpg"
+    "https://i.postimg.cc/HWhHVVXZ/DSC00055-HDR.jpg",
+    "https://i.postimg.cc/W1J420yw/DSC00001-HDR.jpg",
+    "https://i.postimg.cc/Fz8QqBh5/DSC00083-HDR.jpg",
+    "https://i.postimg.cc/KcqcB9S5/DSC00178-Enhanced-NR.jpg",
+    "https://i.postimg.cc/JhMC9JCn/DSC00200-HDR-Edit.jpg",
+    "https://i.postimg.cc/hvfB2BW1/DSC00506-Enhanced-NR.jpg",
+    "https://i.postimg.cc/zGn1t7M6/DSC00522-Enhanced-NR.jpg",
+    "https://i.postimg.cc/rmhTGpbp/DSC00538-Enhanced-NR.jpg",
+    "https://i.postimg.cc/Jh5Mnx5Z/DSC00551-Enhanced-NR.jpg",
+    "https://i.postimg.cc/wjKpQZWD/DSC00553-Enhanced-NR.jpg",
+    "https://i.postimg.cc/vmQCPs5x/DSC00581-Pano-topaz-denoise-sharpen.jpg",
+    "https://i.postimg.cc/y8hwXd3z/DSC00589-Pano-topaz-denoise-sharpen.jpg",
+    "https://i.postimg.cc/KjK6nDB9/DSC00666.jpg",
+    "https://i.postimg.cc/DZzkzTZ3/DSC00702.jpg",
+    "https://i.postimg.cc/DzBHHMcj/DSC00749-Pano.jpg",
+    "https://i.postimg.cc/kMz37LPL/DSC00793.jpg",
+    "https://i.postimg.cc/8Pv8cw1c/DSC00814.jpg",
+    "https://i.postimg.cc/8zD1VxhN/DSC00820-Pano-2.jpg",
+    "https://i.postimg.cc/TYnRFT5t/DSC00840-Pano-2.jpg",
+    "https://i.postimg.cc/9XRXPdm5/DSC00924.jpg",
+    "https://i.postimg.cc/WtLH0L23/DSC00926.jpg",
+    "https://i.postimg.cc/SxrmQVr8/DSC01084-Edit.jpg",
+    "https://i.postimg.cc/PfgXSCJL/DSC01162-HDR.jpg",
+    "https://i.postimg.cc/W41TdrWy/DSC01187-HDR.jpg",
+    "https://i.postimg.cc/zf25KB4G/DSC01222-HDR.jpg",
+    "https://i.postimg.cc/vB1yzydL/DSC01232-HDR.jpg",
+    "https://i.postimg.cc/rmvkZHV6/DSC01253-HDR.jpg",
+    "https://i.postimg.cc/cCwNpcCv/DSC01262-HDR.jpg"
 ]
+
 var thum_id=[0,1,2,3,4]
 
 var big = document.getElementById("big")
-var index = 0
-var tind = 0
+var big_id
 
-var thumbnails = document.getElementById("thumb")
-console.log(thumbnails)
 
-//set big on
-big.src = photo_paths[index]
-
-//set little bois
-var t1 = document.getElementById("0")
-var t2 = document.getElementById("1")
-var t3 = document.getElementById("2")
-var t4 = document.getElementById("3")
-var t5 = document.getElementById("4")
-
-t1.src = photo_paths[tind]
-t2.src = photo_paths[tind+1]
-t3.src = photo_paths[tind+2]
-t4.src = photo_paths[tind+3]
-t5.src = photo_paths[tind+4]
-
-thumbnails.onclick = function thumbs(e){
-    if(e.target.id != "lbuttonlower" && e.target.id !="rbuttonlower"){
-        index = e.target.id
-        console.log(e.target.id)
-        big.src = photo_paths[index]
-    }
-}
-
-//setting up buttons
+//left arrow button
 var left = document.getElementById("lbutton")
 left.onclick = function leftC(){
-    index--;
-    if(index < 0)
-        index += 10
 
-    big.src = photo_paths[index]
+
 }
 
+// righ arrow button
 var right = document.getElementById("rbutton")
 right.onclick = function rightC(){
-    index++;
-    if(index >= photo_paths.length)
-        index -= 10
-    big.src = photo_paths[index]
-    
+
+
 }
 
-var l = document.getElementById("lbuttonlower")
-l.onclick = function lc(){
-    window.alert("832")
+//x button
+var x = document.getElementById("close")
+x.onclick = function close(){
+    window.location.replace("index.html")
 }
 
-var r = document.getElementById("rbuttonlower")
-r.onclick = function lc(){
-    window.alert("ASDAS")
-}
-
-
-function shuffleLeft(){
-    //t1=t2
-    t1.src = t2.src
-    //t2=t3
-    t2.src = t3.src
-    //t3==t4
-    t3.src = t4.src
-    //t4=t5
-    t4.src = t5.src
-    //t5=new
-    t5.src = photo_paths[tind+5]
-    
+function load(){
+    big_id = window.location.hash
+    big_id = big_id.replace("#","")
+    big.src =photo_paths[big_id]
 }
